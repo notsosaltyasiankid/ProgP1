d878f2b9d98be282467d21fef07a9e4db95291b6
Alex <narutovannassau@gmail.com>
2024-09-12 23:13:14 +0200

    opdracht1

---
+++ Assets/Scripts/opdracht 1/CreateBall.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        public GameObject Prefab;
+        private float elapsedTime = 0f;
+        private List<GameObject> balls = new List<GameObject>();
+        private float ballsStart = 100;
+        private GameObject createball(Color c)
+        {
+            GameObject ball = Instantiate(Prefab, new Vector3(UnityEngine.Random.Range(-20,20), UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-20,20)), Quaternion.identity);
+            balls.Add(ball);
+            Material material = ball.GetComponent<MeshRenderer>().material;
+            material.SetColor("_Color", c);
+            DestroyBall();
+            return ball;
+        }
+        public void DestroyBall()
+        {
+            for (int i = 0; i < balls.Count; i++)
+            {
+                Destroy(balls[i],3f);
+            }
+        }
+
+        private void Start()
+        {
+            List<GameObject> Secondballs = new List<GameObject>();
+
+            for (int i = 0; i < ballsStart; i++)
+            {
+
+                float r = UnityEngine.Random.Range(0f, 1f);
+                float g = UnityEngine.Random.Range(0f, 1f);
+                float b = UnityEngine.Random.Range(0f, 1f);
+                Color randColor = new Color(r, g, b, 1f);
+
+                GameObject newBall = createball(randColor);
+                Secondballs.Add(newBall);
+            }
+        }
+        void Update()
+        {
+            float r = UnityEngine.Random.Range(0f, 1f);
+            float g = UnityEngine.Random.Range(0f, 1f);
+            float b = UnityEngine.Random.Range(0f, 1f);
+            Color randColor = new Color(r, g, b, 1f);
+            elapsedTime += Time.deltaTime;
+            if(elapsedTime > 1f)
+            {
+                createball(randColor);
+                elapsedTime = 0f;
+            }
+        }
+    }
---
+++ Assets/Scripts/opdracht 3/EnemySpawner.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    private float wavesize = 100;
+    private List<GameObject> enemylist = new List<GameObject>();
+    private float otherEnemys = 3;
+    private float elapsedTime = 0f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        elapsedTime = 0f;
+    }
+    public void createwave()
+    {
+        
+        for (int i = 0; i < wavesize; i++) 
+        {
+            enemylist.Add(Instantiate(prefab));
+        }
+    }
+
+    public void createThree()
+    {
+        for (int i = 0; i < otherEnemys; i++)
+        {
+            enemylist.Add(Instantiate(prefab));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime = Time.deltaTime;
+
+        if (elapsedTime >= 1f)
+        {
+            createThree();
+            elapsedTime = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            createwave();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            for (int i = 0; i < enemylist.Count; i++)
+            {
+                Destroy(enemylist[i]);
+            }
+            enemylist.Clear();
+        }
+
+
+    }
+    
+}
